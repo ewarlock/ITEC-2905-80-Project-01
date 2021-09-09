4709fbd484309ba02449d1e956e02ee40265a9c4
Clara <10088152+claraj@users.noreply.github.com>
2021-09-09 17:24:00 -0500

    Update RefactorQuiz.py

--- RefactorQuiz.py
+++ RefactorQuiz.py
@@ -1,16 +1,19 @@
-""" This program offers the user a quiz. The quiz can be selected based on several different categories. At the end of the quiz, the user is scored based on how many questions they got right. They receive a special message if they got all of the questions right."""
+""" This program offers the user a quiz. The quiz can be selected based on several different categories. 
+At the end of the quiz, the user is scored based on how many questions they got right. 
+They receive a special message if they got all of the questions right."""
 
-#quiz questions and answers sorted by category
+# A blank space after a # is a little easier to read
+# quiz questions and answers sorted by category
 categories = [
     {
         'id': '1',
         'Name':'Art',
-        'Questions':[
-            {'Question':'Who painted the Mona Lisa?', 'Answer':'Leonardo Da Vinci'},
-            {'Question':'What precious stone is used to make the artist\'s pigment ultramarine?', 'Answer':'Lapiz Lazuli' },
-            {'Question':'Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?', 'Answer':'Chicago'},
-            {'Question':'Which kid\'s TV characters are named after Renaissance artists?', 'Answer':'Teenage Mutant Ninja Turtles'},
-            {'Question':'The graphite in an artist\'s pencil is made of what chemical element?', 'Answer':'Carbon'}
+        'Questions': [   # Python style guides recommends spaces after : in dictionaries
+            {'Question': 'Who painted the Mona Lisa?', 'Answer':'Leonardo Da Vinci'},
+            {'Question': 'What precious stone is used to make the artist\'s pigment ultramarine?', 'Answer':'Lapiz Lazuli' },
+            {'Question': 'Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?', 'Answer':'Chicago'},
+            {'Question': 'Which kid\'s TV characters are named after Renaissance artists?', 'Answer':'Teenage Mutant Ninja Turtles'},
+            {'Question': 'The graphite in an artist\'s pencil is made of what chemical element?', 'Answer':'Carbon'}
         ]
     },
     {   
@@ -37,10 +40,10 @@ categories = [
 
 
 def score_question(user_answer, quiz_answer):
-    #score user answers to each question
+    # score user answers to each question
     score = 0
 
-    #convert to upper for comparison so case doesn't matter
+    # convert to upper for comparison so case doesn't matter
     if user_answer.upper() == quiz_answer.upper():
         print(f'{quiz_answer} is correct!')
         score = 1
@@ -53,7 +56,7 @@ def score_question(user_answer, quiz_answer):
 def run_quiz(category):
     #runs the quiz based on category selected
     score = 0
-    total_questions = len(category['Questions']) #count how many questions in category
+    total_questions = len(category['Questions']) # count how many questions in category
     category_name = category['Name']
 
     #tell user what category they selected
@@ -78,35 +81,49 @@ def run_quiz(category):
 
 
 
-def validate_category(category_selection):
-    #make list of category ids for user input validation
-    category_ids = []
-    for category in categories:
-        category_ids.append(category['id'])
-
+def validate_category(category_ids):
+    """ These could be docstrings to describe the purpse of the function.
+    Use triple double quotes, explain what the function parameters are for, and what this function returns.
+    
+    Ask user to enter category ID, and validate it is one of the choices in the 
+    Return the category ID selected.  
+    
+    """
+    
     total_categories = len(category_ids)
 
     #user is prompted to re enter category id until they enter something valid
+
+    category_selection = ''
+
     while category_selection not in category_ids:
+
+        # useful to explain to the user what they should enter, but can you guarantee that 
+        # you have sequential category numbers? 
         print(f'Invalid selection. Please select a number within the range 1 through {total_categories}.')
         category_selection = input('Please type the number of the category you want to be quizzed on: ')
 
     return category_selection
 
 
-
 def select_category():
-    #prompt user to select a category and pass that selection to get_questions function
-    
-    #print all of the categories alongside IDs for user to select from
+    # this could be replaced with a docstring
+    # prompt user to select a category and pass that selection to get_questions function
+    # print all of the categories alongside IDs for user to select from
+
+    # display list of valid category ids from the categories dictionary 
     for category in categories:
         category_id = category['id']
         category_name = category['Name']
         print(f'{category_id}. {category_name}')
 
-    category_selection = input('Please type the number of the category you want to be quizzed on: ')
+    # You could combine this with the previous loop, if desired. It's fine like this too
+    # I'd only recommend combining if the loops processed a lot of data and were a performance issue
+    category_ids = []
+    for category in categories:
+        category_ids.append(category['id'])
 
-    category_selection = validate_category(category_selection)
+    category_selection = validate_category(category_ids)
 
     #select category based on id entered by user
     for category in categories:
@@ -114,7 +131,6 @@ def select_category():
             category_selection = category
 
     return category_selection
-
 
 
 def main():
@@ -125,6 +141,7 @@ def main():
     run_quiz(category)
 
     print('Thanks for using this Quiz Program!')
+
 
 #call main to start program
 if __name__ == '__main__':
